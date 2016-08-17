c92d3923066cf16ebc5018290796fe1afbc350c2
Ryan Huang <ryan_huang@intuit.com>
2016-08-17 15:28:22 -0700

    link panel photos

--- app/components/LinkPanel.jsx
+++ app/components/LinkPanel.jsx
@@ -8,23 +8,27 @@ class LinkPanel extends Component {
   render() {
     return (
       <div className="link-panel-container">
-        <div class="icon-bar one-up link-image">
-          <h5>Visit our Facebook webpage for more information</h5>
-          <a class="item" href="https://www.facebook.com/profile.php?id=100005110844639">
-            <img src="http://cdn.gsmarena.com/pics/12/05/facebook-for-android-updated/big.jpg" />
-          </a>
-        </div>
-        <div className="link-pannel-image1">
-          <img  src={'https://scontent.fsnc1-3.fna.fbcdn.net/t31.0-8/12916085_555755484604808_1981014369933748534_o.jpg'}
-            alt="banner"/>
-        </div>
-        <div className="link-pannel-image2">
-          <img  src={'https://scontent.fsnc1-3.fna.fbcdn.net/t31.0-8/12967908_555755164604840_653809978844213369_o.jpg'}
-            alt="banner"/>
-        </div>
-        <div className="link-pannel-image3">
-          <img  src={'https://scontent.fsnc1-3.fna.fbcdn.net/t31.0-8/12916932_551431438370546_7283733245991238353_o.jpg'}
-            alt="banner"/>
+          <div className="link-group">
+            <a class="button" href="https://www.facebook.com/profile.php?id=100005110844639">
+              <img src="http://harousa.org/wp-content/uploads/2015/06/facebook-icon-2-200x200.png" />
+            </a>
+            <a class="button" href="https://www.instagram.com/cpslokasa/?hl=en">
+              <img src="http://3835642c2693476aa717-d4b78efce91b9730bcca725cf9bb0b37.r51.cf1.rackcdn.com/Instagram_App_Large_May2016_200.png" />
+            </a>
+          </div>
+        <div className="link-images-container">
+          <div className="link-pannel-image1">
+            <img  src={'https://scontent.fsnc1-3.fna.fbcdn.net/t31.0-8/12916085_555755484604808_1981014369933748534_o.jpg'}
+              alt="banner"/>
+          </div>
+          <div className="link-pannel-image2">
+            <img  src={'https://scontent.fsnc1-3.fna.fbcdn.net/t31.0-8/12967908_555755164604840_653809978844213369_o.jpg'}
+              alt="banner"/>
+          </div>
+          <div className="link-pannel-image3">
+            <img  src={'https://scontent.fsnc1-3.fna.fbcdn.net/t31.0-8/12916932_551431438370546_7283733245991238353_o.jpg'}
+              alt="banner"/>
+          </div>
         </div>
       </div>
     );
