2cfc51e708c32eccf4813f172b0a9e64ac04a9e3
Ryan Huang <ryan_huang@intuit.com>
2016-08-18 15:45:22 -0700

    env variables

--- app/firebase/index.js
+++ app/firebase/index.js
@@ -2,10 +2,10 @@ import firebase from 'firebase';
 
 try {
   const config = {
-      apiKey: "AIzaSyDm0SVmHECXWSolqr2J0N1mC8Sh7yj2wUQ",
-      authDomain: "cpkasa-test.firebaseapp.com",
-      databaseURL: "https://cpkasa-test.firebaseio.com",
-      storageBucket: "cpkasa-test.appspot.com",
+      apiKey: process.env.API_KEY,
+      authDomain: process.env.AUTH_DOMAIN,
+      databaseURL: process.env.DATABASE_URL,
+      storageBucket: process.env.STORAGE_BUCKET,
     };
 
   firebase.initializeApp(config);
